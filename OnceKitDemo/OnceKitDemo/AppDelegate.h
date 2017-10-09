ea3bbbbe8d30425f02e8255313e3bf2494f2725a
曹绍奇 <caoshaoqi@caoshaoqis-MacBook-Pro.local>
2017-10-09 18:25:20 +0800

    add podspec file

---
+++ OnceKitDemo/OnceKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnceKitDemo
+//
+//  Created by 曹绍奇 on 2017/10/9.
+//  Copyright © 2017年 曹绍奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
